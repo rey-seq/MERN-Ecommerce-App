ee5c3275a5efe92a2e6cd31fd7e06de930677b4a
rey_seq <reynold.seq@gmail.com>
2024-10-26 16:20:51 +0530

    chore:(client) setup

---
+++ client/src/providers/index.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from "@/context/theme-provider";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider>{children}</ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
